93b13718e62234239be7753169b94755596562eb
hind <hind@192.168.30.129>
2018-06-03 22:49:58 +0200

    Initial Commit

---
+++ WeatherTableViewController.swift
@@ -0,0 +1,95 @@
+//
+//  WeatherTableViewController.swift
+//  weather App
+//
+//  Created by hind on 3/28/18.
+//  Copyright © 2018 hind. All rights reserved.
+//
+
+import UIKit
+import CoreLocation
+
+class WeatherTableViewController: UITableViewController , UISearchBarDelegate{
+
+    
+    @IBOutlet weak var SearchBar: UISearchBar!
+
+     var forecastData = [Weather]()
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+       SearchBar.delegate = self
+        
+        updateWeatherForLocation(location: "New York")
+        
+  
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    // MARK: - Table view data source
+
+   
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        searchBar.resignFirstResponder()
+        if let locationstring = searchBar.text,!locationstring.isEmpty{
+    
+         updateWeatherForLocation(location: locationstring)
+        }
+    }
+    
+    func updateWeatherForLocation (location:String) {
+        CLGeocoder().geocodeAddressString(location) { (placemarks:[CLPlacemark]?, error:Error?) in
+            if error == nil {
+                if let location = placemarks?.first?.location {
+                    Weather.forecast(withLocation: location.coordinate, completion: { (results:[Weather]?) in
+                        
+                        if let weatherData = results {
+                            self.forecastData = weatherData
+                            
+                            DispatchQueue.main.async {
+                                self.tableView.reloadData()
+                            }
+                            
+                        }
+                        
+                    })
+                }
+            }
+        }
+    
+}
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return forecastData.count
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        let date = Calendar.current.date(byAdding: .day, value: section, to: Date())
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "MMMM dd, yyyy"
+        
+        return dateFormatter.string(from: date!)
+    }
+    
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        
+        let weatherObject = forecastData[indexPath.section]
+        
+        cell.textLabel?.text = weatherObject.summary
+        cell.detailTextLabel?.text = "\(Int(weatherObject.temperature)) °F"
+        cell.imageView?.image = UIImage(named: weatherObject.icon)
+        
+        return cell
+    }
+}
---
+++ weather App/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  weather App
+//
+//  Created by hind on 3/27/18.
+//  Copyright © 2018 hind. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
